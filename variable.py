e29d86aa247b9f7dfe28ca7cd6443168d3899326
Carl00000000 <60214375+Carl00000000@users.noreply.github.com>
2025-11-29 23:31:24 +0800

    1

---
+++ variable.py
@@ -0,0 +1,18 @@
+class Variable
+    def __init__(self,data):
+        self.data=data
+        self.grad=None  #导数
+        self.creator=None #创造者
+
+    def backward(self):
+        funcs=[self.creator]
+        while funcs:
+            f=funcs.pop()
+            if type(f) is list
+                funcs.extend(f)
+            else:
+                x,y=f.input,f.output
+                x.grad=f.backward(y.grad)
+
+                if x.creator is not None:
+                    funcs.append(x.creator)
