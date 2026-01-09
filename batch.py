a88392d6bb7959cc4a624742d8aae0a7ef3519b3
Carl00000000 <60214375+Carl00000000@users.noreply.github.com>
2026-01-09 21:13:19 +0800

    1

--- batch.py
+++ batch.py
@@ -1,4 +1,4 @@
-def batch(self,a,b,c):
+def batch_X2(self,a,b,c):
     z=[]
     for i in len(a):
         a1=a[i]
@@ -6,4 +6,10 @@ def batch(self,a,b,c):
         z1=c(a1,b1)
         z.append(z1)
 
+    return z
+
+def batch_add_X1(self,a):
+    z=0.0
+    for i in len(a):
+        z=z+a[i]
     return z
